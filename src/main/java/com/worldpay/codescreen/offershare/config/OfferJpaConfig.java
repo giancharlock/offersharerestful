0d54e04a81b241aa77b15b0a45a959f535ab33bc
Giancarlo Cadei <giancarlo.cadei@mapsgroup.it>
2019-02-10 20:43:36 +0100

    little issues

--- src/main/java/com/worldpay/codescreen/offershare/config/OfferJpaConfig.java
+++ src/main/java/com/worldpay/codescreen/offershare/config/OfferJpaConfig.java
@@ -1,6 +1,5 @@
 package com.worldpay.codescreen.offershare.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
--- src/main/java/com/worldpay/codescreen/offershare/pojo/Offer.java
+++ src/main/java/com/worldpay/codescreen/offershare/pojo/Offer.java
@@ -1,7 +1,6 @@
 package com.worldpay.codescreen.offershare.pojo;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.io.Serializable;
